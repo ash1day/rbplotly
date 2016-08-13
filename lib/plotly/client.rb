1bf353c07a7b0e33e8434f094bb2d88478778561
y4ashida <y4ashida@gmail.com>
2016-08-13 15:56:02 +0900

    Add client methods about plotting a image

--- lib/plotly/client.rb
+++ lib/plotly/client.rb
@@ -1,13 +1,47 @@
 require 'faraday'
-require 'faraday_middleware'
+require 'base64'
+require 'json'
 
 module Plotly
   class Client
     # @param username [String]
     # @param api_key [String]
-    def initialize(username, api_key)
-      @username = username
-      @api_key = api_key
+    def initialize(username: nil, api_key: nil)
+      @conn = Faraday.new(
+        url: 'https://api.plot.ly/v2',
+        headers: build_headers(username, api_key)
+      )
+    end
+
+    # @param data [Array]
+    # @param image_path [String]
+    # @param image_type [String]
+    # @param layout [hash]
+    def plot_image(data, image_path, image_type, layout = {})
+      payload = {
+        figure: {
+          data: data,
+          layout: layout
+        },
+        format: image_type
+      }.to_json
+
+      res = @conn.post('images', payload)
+      IO.binwrite(image_path.to_s, res.body)
+    end
+
+    private
+
+    # @param username [String]
+    # @param api_key [String]
+    # @return [Hash] HTTP headers
+    def build_headers(username, api_key)
+      encoded_auth = Base64.encode64("#{username}:#{api_key}")
+      {
+        'plotly-client-platform' => "Ruby #{Plotly::VERSION}",
+        'content-type' => 'application/json',
+        authorization: "Basic #{encoded_auth}"
+      }
     end
   end
 end
