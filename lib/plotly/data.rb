b547a0fa4206ce6d441404ef4ae1d1ecf04eba1d
y4ashida <y4ashida@gmail.com>
2016-08-21 01:15:00 +0900

    Add attributes to Data and Layout class

--- lib/plotly/data.rb
+++ lib/plotly/data.rb
@@ -1,12 +1,18 @@
 module Plotly
   class Data
-    attr_accessor :x, :y, :type, :mode
+    attr_accessor :x, :y, :type, :mode, :name
 
-    def initialize(x: nil, y: nil, type: nil, mode: nil)
+    # @option x [Array]
+    # @option y [Array]
+    # @option type [String]
+    # @option mode [String]
+    # @option name [String]
+    def initialize(x: nil, y: nil, type: nil, mode: nil, name: nil)
       @x = x
       @y = y
       @type = type
       @mode = mode
+      @name = name
     end
 
     def to_h
@@ -14,7 +20,8 @@ module Plotly
         x: @x,
         y: @y,
         type: @type,
-        mode: @mode
+        mode: @mode,
+        name: @name
       }
     end
   end
--- spec/plotly/layout_spec.rb
+++ spec/plotly/layout_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 
 describe Plotly::Layout do
-  let(:layout) { Plotly::Layout.new }
+  let(:layout) { Plotly::Layout.new(params) }
 
-  it 'can be instantiated' do
-    expect(layout).to be_a Plotly::Layout
+  describe '#initialize' do
+    context 'with no params' do
+      let(:params) { Hash.new }
+      it { expect(layout).to be_a Plotly::Layout }
+    end
+
+    context 'with params' do
+      let(:params) { { barmode: 'stack' } }
+      it { expect(layout).to be_a Plotly::Layout }
+    end
   end
 end
