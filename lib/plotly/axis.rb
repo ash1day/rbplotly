a622af8a14e97c6b444b49bd0e127d5d53c3dae3
ash1day <y4ashida@gmail.com>
2016-09-06 23:55:46 +0900

    Add an attribute to Axis

--- lib/plotly/axis.rb
+++ lib/plotly/axis.rb
@@ -4,9 +4,12 @@ module Plotly
   class Axis
     include Castable
 
-    ATTRS = %i(title range).freeze
+    ATTRS = %i(title range zeroline).freeze
     attr_accessor(*ATTRS)
 
+    # @option opts [String] title
+    # @option opts [Array] range
+    # @option opts [Boolean] zeroline
     def initialize(opts)
       return unless opts.is_a?(Hash)
       opts.each { |k, v| instance_variable_set("@#{k}", v) }
