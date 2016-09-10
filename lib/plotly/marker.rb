929d58749c09b089d7b56fc7f2e4435d39e698dd
ash1day <y4ashida@gmail.com>
2016-09-10 11:16:28 +0900

    Fix namespace

--- lib/plotly/marker.rb
+++ lib/plotly/marker.rb
@@ -10,12 +10,12 @@ module Plotly
 
     # @option opts [Fixnum] size
     # @option opts [String] color e.g.'rgba(0, 0, 200, .5)'
-    # @option opts [Hash or Plotly::Data::Marker::Line] line
+    # @option opts [Hash or Plotly::Line] line
     def initialize(opts)
       return unless opts.is_a?(Hash)
 
       opts.each do |k, v|
-        v.convert_to(Plotly::Data::Marker::Line) if k.to_sym == :line
+        v.convert_to(Plotly::Line) if k.to_sym == :line
         instance_variable_set("@#{k}", v)
       end
     end
